917eb712cef40e231c7b5b235fe372b8836f625f
mungsehari <mungse.hari@gmail.com>
2024-11-16 20:40:22 +0530

    create book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,10 +1,12 @@
 import path from "node:path";
+import fs from "node:fs";
 import { NextFunction, Request, Response } from "express";
 import cloudinary from "../config/cloudinary";
 import createHttpError from "http-errors";
+import bookModel from "./bookModel";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("file", req.files);
+  const { title, genre } = req.body;
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
   const coverImgaseMimeType = files.converImgae[0].mimetype.split("/").at(-1);
 
@@ -40,11 +42,25 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
     );
     console.log("bookFileUploadResult", bookFileUploadResult);
     console.log("uploadResult", uploadResult);
+
+    const newBook = await bookModel.create({
+      title,
+      genre,
+      author: "673819fea87420e3243c48f5",
+      converImgae: uploadResult.secure_url,
+      file: bookFileUploadResult.secure_url,
+    });
+    // delete file
+    try {
+      await fs.promises.unlink(filePath);
+      await fs.promises.unlink(bookFilePath);
+    } catch (error) {
+      return next(createHttpError(500, "Error while deleting file"));
+    }
+    res.status(201).json({ id: newBook._id });
   } catch (error) {
     return next(createHttpError(500, "Error while uploading file"));
   }
-
-  res.json({ message: "create book" });
 };
 
 export { createBook };
