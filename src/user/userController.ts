5adb3d66d51f99c00eed89586b93d4353900b5cf
mungsehari <mungse.hari@gmail.com>
2024-11-16 13:22:28 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -51,5 +51,27 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(createHttpError(500, "Error while signing the jwt token"));
   }
 };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
 
-export { createUser };
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    return next(createHttpError(400, "User not found"));
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return next(createHttpError(400, "Invalid credentials"));
+  }
+
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
+  res.status(201).json({ accessToken: token });
+};
+
+export { createUser, loginUser };
