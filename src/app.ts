bee33c942adc3cc9dcf7f552f193ed7d001e6b87
mungsehari <mungse.hari@gmail.com>
2024-11-17 19:07:47 +0530

    add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import golbalErrorHandler from "./middlewares/globalErrorHandler";
+import cors from "cors";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRouter";
+import { config } from "./config/config";
 
 const app = express();
+app.use(
+  cors({
+    origin: config.frontendDomin,
+  })
+);
 app.use(express.json());
 app.get("/", (req, res, next) => {
   res.json({ message: "welcome to elib apis" });
