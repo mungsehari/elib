6814bb69f571845a90d0160153c4dfa31d6832e5
mungsehari <mungse.hari@gmail.com>
2024-11-18 18:00:02 +0530

    add populate

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -141,7 +141,7 @@ const updateBook = async (req: Request, res: Response, next: NextFunction) => {
 
 const listBooks = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const book = await bookModel.find();
+    const book = await bookModel.find().populate("author", "name");
     res.json(book);
   } catch (error) {
     return next(createHttpError(500, "Error while listing books"));
@@ -154,7 +154,9 @@ const getSingleBook = async (
 ) => {
   const bookId = req.params.bookId;
   try {
-    const book = await bookModel.findOne({ _id: bookId });
+    const book = await bookModel
+      .findOne({ _id: bookId })
+      .populate("author", "name");
     if (!book) {
       return next(createHttpError(404, "Book not found"));
     }
